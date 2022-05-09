c4a9546e6ec67818268f85785ef3e58c5c57a0c1
Lixy0 <lixygame@gmail.com>
2022-05-09 23:55:16 +0200

    haha je suis stupide mais c'est bon normalement
    :')

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,68 +1,62 @@
+#include <string>
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <map>
+
 
 using std::ifstream;
-using std::string;
 
 using namespace std;
 
-struct Page
+/* //--- TEST ---//
+* 
+#include <fstream>
+
+
+size_t countOccurrences(char c, string& str)
 {
-    string val;
-    Page* suivant;
-};
+    size_t count = 0;
+    char tmp = static_cast<char>(tolower(static_cast<unsigned char>(c)));
 
+    for (char i : str)
+        if (tolower(static_cast<unsigned char>(i)) == tmp)
+            count++;
 
-Page* creerliste()
-{
-    ifstream entree("wildwildwestlyrics.txt");
-    string i;
-    Page* premier;
-    Page* precedent;
-    Page* nouveau;
-
-    premier = new Page;
-    entree >> premier->val;
-    precedent = premier;
-    while (entree >> i)
-    {
-        nouveau = new Page;
-        precedent->suivant = nouveau;
-        precedent = nouveau;
-        nouveau->val = i;
-    }
-    precedent->suivant = 0;
-
-    return premier;
+    return count;
 }
 
-void compare(Page* debut)
-{
-    int i = 0;
-    ifstream ifs("wildwildwestlyrics.txt");
-    string s;
-    Page* head = debut;
 
-    while (ifs >> s)
-    {
-        debut = head;
-        while (debut != 0)
-        {
-            if (s == debut->val)
-                i = i + 1;
-            debut = debut->suivant;
-        }
+int main() {
 
-        cout << "le nombre d'occurence du mot " << s << " est " << i << endl;
-        i = 0;
-    }
+    fstream lyrics;
+    lyrics.open("wildwildwestlyrics.txt");
+
+    char ch1 = 'w';
+    string str1(lyrics);
+
+    cout << "number of character - '" << ch1 << "' occurrences = " << countOccurrences(ch1, str1) << endl;
+
+    exit(EXIT_SUCCESS);
 }
+*
+* 
+*/
+
+
 
 int main()
 {
-    Page* premier = creerliste();
-    compare(premier);
-    system("pause");
-    return 0;
+ifstream ifs("wildwildwestlyrics.txt");
+string s;
+map<string, int> map;
+
+while (ifs >> s)
+++map[s];
+
+typedef std::map<std::string, int>::const_iterator iter;
+for (iter it = map.begin(); it != map.end(); ++it)
+std::cout << "le nombre d'occurence du mot " << it->first << " est " << it->second << std::endl;
+
+return 0;
 }
